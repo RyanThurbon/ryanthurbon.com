5ba936b72a124a233e81771fb5d80f640e36306e
Ryan Thurbon <ryanthurbon3@gmail.com>
2025-09-02 09:38:20 +0200

    change portait image

--- src/components/features/about/portrait.tsx
+++ src/components/features/about/portrait.tsx
@@ -2,10 +2,8 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar.tsx"
 
 export function AboutPortrait() {
     return (
-        <Avatar
-            className="rounded-2xl size-75 lg:size-105 rotate-3 pointer-events-none select-none"
-        >
-            <AvatarImage loading="lazy" decoding="async" src="/portrait.jpeg" alt="Portait of Ryan Thurbon"/>
+        <Avatar className="rounded-2xl size-75 lg:size-105 rotate-3 pointer-events-none select-none">
+            <AvatarImage loading="lazy" decoding="async" src="/portrait-copy.jpg" alt="Portait of Ryan Thurbon" />
             <AvatarFallback>Portrait of Ryan Thurbon</AvatarFallback>
         </Avatar>
     );
