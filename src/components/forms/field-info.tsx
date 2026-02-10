f471cfbb68cc8742d5aa518d08670f3e2014e501
Ryan Thurbon <ryanthurbon3@gmail.com>
2026-02-10 22:33:13 +0200

    fix: form error text

--- src/components/forms/field-info.tsx
+++ src/components/forms/field-info.tsx
@@ -3,7 +3,7 @@ import { AnyFieldApi } from "@tanstack/form-core";
 export function FieldInfo({ field }: { field: AnyFieldApi }) {
     if (field.state.meta.isTouched && !field.state.meta.isValid) {
         return field.state.meta.errors.map((error) => (
-            <em role="alert" key={error.message}>
+            <em role="alert" key={error.message} className="text-destructive">
                 {error.message}
             </em>
         ));
